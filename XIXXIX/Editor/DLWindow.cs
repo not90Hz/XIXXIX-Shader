530b0b776ed8e54779ca7e6862335aca668f9ccb
SESOSAS <87881617+not90Hz@users.noreply.github.com>
2022-06-18 00:54:12 +0200

    Update DLWindow.cs

--- XIXXIX/Editor/DLWindow.cs
+++ XIXXIX/Editor/DLWindow.cs
@@ -36,20 +36,23 @@ namespace XIXXIX
 
         void OnGUI()
         {
-            EditorGUILayout.BeginHorizontal(GUI.skin.box);
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            EditorGUILayout.LabelField("Version:", Version + " - " + LatestVersion);
             if (GUILayout.Button("Download")) Download(); 
-            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.EndVertical();
         }
 
         void Download()
         {
             WebClient webClient = new WebClient();
-            webClient.DownloadFile("https://github.com/not90Hz/XIXXIX-Shader/releases/latest/", string.Format("{0}.unitypackage", FileName));
+            Application.OpenURL("https://github.com/not90Hz/XIXXIX-Shader/releases/latest/download/" + string.Format("{0}.unitypackage", FileName));
+            //webClient.DownloadFile("https://github.com/not90Hz/XIXXIX-Shader/releases/latest/download/" + string.Format("{0}.unitypackage", FileName), string.Format("{0}.unitypackage", FileName));
             string downloads = Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders", "{374DE290-123F-4565-9164-39C4925E467B}", String.Empty).ToString();
             if (File.Exists(downloads + string.Format("/{0}.unitypackage", FileName)))
             {
                 if (File.Exists(string.Format("{0}{1}.unitypackage", InstallPath, FileName))) File.Delete(string.Format("{0}{1}.unitypackage", InstallPath, FileName));
                 File.Move(downloads + string.Format("/{0}.unitypackage", FileName), string.Format("{0}{1}.unitypackage", InstallPath, FileName));
+                AssetDatabase.ImportPackage(string.Format("{0}{1}.unitypackage", InstallPath, FileName), true);
             }
         }
     }
